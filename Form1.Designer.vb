9357986f6adec453aecbf1766685736838c0b955
[jan <jageraldez20@gmail.com>
2024-07-16 22:46:37 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBoxSum = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBoxProduct = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBoxDifference = New System.Windows.Forms.MaskedTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBoxQuotient = New System.Windows.Forms.MaskedTextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(77, 73)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First Number"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 105)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Second Number"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(80, 349)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "DISPLAY"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(188, 349)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "CLEAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(296, 349)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 28)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "CLOSE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(169, 70)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(169, 102)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(132, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBoxSum
+        '
+        Me.TextBoxSum.Location = New System.Drawing.Point(167, 192)
+        Me.TextBoxSum.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxSum.Name = "TextBoxSum"
+        Me.TextBoxSum.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxSum.TabIndex = 8
+        '
+        'TextBoxProduct
+        '
+        Me.TextBoxProduct.Location = New System.Drawing.Point(167, 224)
+        Me.TextBoxProduct.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxProduct.Name = "TextBoxProduct"
+        Me.TextBoxProduct.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxProduct.TabIndex = 9
+        '
+        'TextBoxDifference
+        '
+        Me.TextBoxDifference.Location = New System.Drawing.Point(167, 256)
+        Me.TextBoxDifference.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxDifference.Name = "TextBoxDifference"
+        Me.TextBoxDifference.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxDifference.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(117, 196)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "SUM"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(79, 228)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 16)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "PRODUCT"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(60, 260)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 16)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "DIFFERENCE"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(75, 294)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 16)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "QUOTIENT"
+        '
+        'TextBoxQuotient
+        '
+        Me.TextBoxQuotient.Location = New System.Drawing.Point(167, 290)
+        Me.TextBoxQuotient.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxQuotient.Name = "TextBoxQuotient"
+        Me.TextBoxQuotient.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxQuotient.TabIndex = 14
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(166, 172)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "RESULTS:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 423)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBoxQuotient)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxDifference)
+        Me.Controls.Add(Me.TextBoxProduct)
+        Me.Controls.Add(Me.TextBoxSum)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As MaskedTextBox
+    Friend WithEvents TextBox2 As MaskedTextBox
+    Friend WithEvents TextBoxSum As MaskedTextBox
+    Friend WithEvents TextBoxProduct As MaskedTextBox
+    Friend WithEvents TextBoxDifference As MaskedTextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBoxQuotient As MaskedTextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Dim x, y, ans As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+
+        ans = x + y
+        TextBoxSum.Text = ans
+
+        ans = x * y
+        TextBoxProduct.Text = ans
+
+        ans = x - y
+        TextBoxDifference.Text = ans
+
+        ans = x / y
+        TextBoxQuotient.Text = ans
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Text = " "
+        TextBox2.Text = " "
+        TextBoxSum.Text = " "
+        TextBoxProduct.Text = " "
+        TextBoxDifference.Text = " "
+        TextBoxQuotient.Text = " "
+
+    End Sub
+
+    Private Sub TextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles TextBox1.MaskInputRejected
+
+    End Sub
+
+
+    Private Sub MaskedTextBox1_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles TextBoxQuotient.MaskInputRejected
+
+    End Sub
+End Class
